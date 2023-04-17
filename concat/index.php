b3bdd4b5423527e87050e7fa1aae529cf65ddc3b
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-04-17 20:51:11 +0200

    Creating new object

--- concat/index.php
+++ concat/index.php
@@ -5,10 +5,12 @@ class concationationArray
     public function concatArray(array $array1): void
     {
         $result = "";
-        for($i=0; count($array1)> $i; $i++)
-        {
+        for ($i = 0; count($array1) > $i; $i++) {
             $result = $result . $array1[$i];
         }
         echo $result;
     }
 }
+$array1 = ["asfsd", "dsfaa", "ADFSG", "1q423"];
+$concationationObject = new concationationArray();
+$concationationObject->concatArray($array1);
